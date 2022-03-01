d366e08062586b392863bdd563e6f8be4ff64168
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-03-01 17:26:20 -0300

    Operations BUG FIXED * -> subtract function was adding instead subtract

--- src/operations.rs
+++ src/operations.rs
@@ -7,7 +7,7 @@ pub mod operations {
 
     pub mod subtraction {
         pub fn subtract(number_one : f32, number_two : f32) -> f32 {
-            number_one + number_two
+            number_one - number_two
         }
     }
 } 
