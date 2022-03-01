b6dd4443b718e658c5594cc69434b1171afd6dd0
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-03-01 14:11:42 -0300

    Main class -> multiply function created;
    multiply fn -> multiply two numbers and return (f32) the result

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ fn main() {
     let subtract_result = subtract(5f32, 4f32);
 
     println!("{}", subtract_result);
+
+    let multiply_result = multiply(5f32, 5f32);
+
+    println!("{}", multiply_result);
 }
 
 fn add(number_one : f32, number_two : f32) -> f32 {
@@ -15,4 +19,8 @@ fn add(number_one : f32, number_two : f32) -> f32 {
 
 fn subtract(number_one : f32, number_two : f32) -> f32 {
     number_one - number_two
+}
+
+fn multiply(number_one : f32, number_two : f32) -> f32 {
+    number_one * number_two
 }
