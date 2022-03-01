53ea49ab2bbed5091c0390126474665fbb9710d8
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-03-01 14:06:13 -0300

    Main class ->  subtract function created
    subtract fn -> subtract two number and return (f32) the result

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,16 @@ fn main() {
     let add_result = add(5f32, 5f32);
 
     println!("{}", add_result);
+
+    let subtract_result = subtract(5f32, 4f32);
+
+    println!("{}", subtract_result);
 }
 
 fn add(number_one : f32, number_two : f32) -> f32 {
     number_one + number_two
+}
+
+fn subtract(number_one : f32, number_two : f32) -> f32 {
+    number_one - number_two
 }
