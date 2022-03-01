f0302092fb753c45bba8472839fa2abe46b678c8
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-03-01 14:00:56 -0300

    Main class -> add function created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+
+    let add_result = add(5f32, 5f32);
+
+    println!("{}", add_result);
+}
+
+fn add(number_one : f32, number_two : f32) -> f32 {
+    number_one + number_two
 }
