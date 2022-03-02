d4baf7fe1f7e6bdc02e4a9c8c0110f571bb83682
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-03-01 21:34:35 -0300

    division class -> divide fn created

--- src/operations.rs
+++ src/operations.rs
@@ -1,12 +1,4 @@
 pub mod addition;
 pub mod subtraction;
 pub mod multiplication;
-
-pub mod operations {
-
-    pub mod division {
-        pub fn divide(number_one : f32, number_two : f32) -> f32 {
-            number_one / number_two
-        }
-    }
-} 
+pub mod division;
---
+++ src/operations/division.rs
@@ -0,0 +1,3 @@
+pub fn divide(number_one : f32, number_two : f32) -> f32 {
+    number_one / number_two
+}
